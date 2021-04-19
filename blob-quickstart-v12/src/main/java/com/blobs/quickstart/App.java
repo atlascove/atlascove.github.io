bfc7ad855940c9c2ef2927675becc300dfb7fb9a
Edoardo Neerhut <eneerhut@gmail.com>
2021-04-18 22:39:28 -0700

    Add Azure blob for Java

---
+++ blob-quickstart-v12/src/main/java/com/blobs/quickstart/App.java
@@ -0,0 +1,15 @@
+package com.blobs.quickstart;
+
+/**
+ * Azure blob storage v12 SDK quickstart
+ */
+import com.azure.storage.blob.*;
+import com.azure.storage.blob.models.*;
+import java.io.*;
+
+public class App
+{
+    public static void main( String[] args ) throws IOException
+    {
+    }
+}
