1e55fd2c9f9acd5f822ad2af984f864c59ac534a
Christopher Beddow <christopher.beddow@gmail.com>
2022-04-10 00:56:13 +0200

    feat: add alternative view

---
+++ cam2/js/capture.js
@@ -0,0 +1,232 @@
+var currentTag;
+var currentCenter;
+var currentNote;
+var mapactive = 1;
+var somethingSelected = 0;
+var currentImage;
+var currentJSON;
+var originalCoords;
+var currentOSMID;
+var currentLayer = 1;
+var currentData = {};
+
+function openMap(){
+ $('#map').css('height', '40vh');
+ $('#gui_controls').css('height','0%');
+ $('#submit_button').show();
+ $('#attribution').css('display', 'block');
+}
+
+function showImage(){
+ $('#vid_container').css('height','60%');
+}
+
+function closeControls(){
+  $('#takePhotoButton').hide();
+  $('gui_controls').hide();
+}
+
+function closeVideo(){
+  $('#video_overlay').hide();
+  $('#video').hide();
+}
+
+function createMetaData(){
+  var observer = marker1.getLngLat();
+  var target = currentCenter;
+  var timestamp = new Date();
+  var epoch = timestamp.getTime() / 1000
+  var tag = currentTag;
+  var hash = Math.random().toString(16).substring(2);
+  var output_data = {
+    "type": "Feature",
+      "properties": {
+        "id": hash,
+        "relation": "views",
+        "object" : currentOSMID.toString(),
+        "username" : "default_user",
+        "tags_primary" : tag,
+        "tags_secondary": tag,
+        "timestamp": timestamp,
+        "camera_params": {
+          "make": "Google",
+          "model": "Pixel 5a",
+          "focal_length": 25,
+          "horizonal_angle" : 72,
+          "compass_angle": 135
+        },
+        "target_coords": [
+          target.lng,
+          target.lat
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          observer.lng,observer.lat
+        ]
+      }
+    }
+  var obj = {
+    "observer": [observer.lng,observer.lat],
+    "target": [target.lng,target.lat],
+    "timestamp": timestamp,
+    "epoch" : epoch,
+    "tag" : tag
+  }
+  currentJSON = output_data;
+}
+
+function getOSMGeom(osmid,type){
+  var url = 'https://overpass-api.de/api/interpreter?data=[out:json];' + type + '(' + osmid + ');convert item ::=::,::geom=geom(),_osm_type=type();out geom;';
+  $.getJSON(url, function (data) {
+    console.log(data);
+    try {
+      map.removeLayer('selected');
+      map.removeSource('selected');
+    } catch(err) {
+      // do nothing
+    }
+    map.addSource('selected', {
+      'type': 'geojson',
+      'data': {
+        'type': 'Feature',
+        'geometry': data['elements'][0]['geometry']
+      }
+    });
+    if(type == 'way'){
+      map.addLayer({
+        'id': 'selected',
+        'type': 'line',
+        'source': 'selected',
+        'paint': {
+          'line-color': 'red',
+          'line-width': 3
+        }
+      });
+    }
+    if(type == 'node'){
+      map.addLayer({
+        'id': 'selected',
+        'type': 'circle',
+        'source': 'selected',
+        'paint': {
+          'circle-color': 'red'
+        }
+      });
+    }
+  })
+}
+
+
+
+function getOSMData(){
+  $('#linktable').empty();
+  currentData = {};
+  var coords = currentCenter;
+  var bbox = turf.bbox(turf.buffer(turf.point([coords.lng,coords.lat]),15, {units: 'meters'}))
+  var url = 'https://api.openstreetmap.org/api/0.6/map.json?bbox=' + bbox.toString();
+  console.log(url);
+  $.getJSON(url, function (data) {
+    console.log(data);
+    for(e=0; e < data['elements'].length; e++){
+      var el = data['elements'][e]
+      if(el.hasOwnProperty('tags')){
+        currentData[el['id']] = el;
+        console.log(el);
+        var row = '<tr><td class="tag" id="'+ el['id'] + '">' + JSON.stringify(el['tags']) + '</td></tr>';
+        $('#linktable').append(row);
+      }
+    }
+    if (data['elements'].length == 0){
+        var row = '<tr><td>Tap blue to add note: no OSM tags found</td>';
+        $('#linktable').append(row);
+    }
+  });
+}
+
+function updateWorld(obj){
+  var dbx = new Dropbox.Dropbox({ accessToken: 'vmLipsZvDEMAAAAAAAAAAdxF-uncM-lEPVDrE-hJzGcVg-ljIlFLPGl-QUNCpqXJ' });
+  dbx.sharingGetSharedLinkFile({url: 'https://www.dropbox.com/s/35s2yx3qnmcy8ks/world.geojson?dl=0'})
+    .then(function(data) {
+      var downloadUrl = URL.createObjectURL(data.fileBlob);
+      $.ajax({
+          type: 'GET',
+          url: downloadUrl
+      }).done(function(data) {
+        jdata = JSON.parse(data);
+        jdata['features'].push(obj)
+        dbx.filesUpload({ path: '/atlascove/world.geojson', contents: JSON.stringify(jdata), mode:'overwrite' })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+    return false;
+  })
+}
+
+function upload2(img,json){
+  var hash = JSON.parse(json)['properties']['id'];
+  var url = 'https://atlascove.blob.core.windows.net/images/' + hash + '.png?sp=racwdl&st=2021-04-24T19:54:54Z&se=2022-04-25T03:54:54Z&spr=https&sv=2020-02-10&sr=c&sig=nwILb9g1i%2BOB415atZOAfjTOY8KCmfzOLPg46Ut7aXQ%3D';
+  $.ajax({
+      type: 'PUT',
+      url: url,
+      data: img,
+      processData: false,
+      contentType: false,
+      headers: {"Content-Type": "image/png", "x-ms-blob-type": "BlockBlob" }
+  }).done(function(data, xhr) {
+         console.log(data);
+         var status = xhr.status;
+         console.log(status);
+  });
+  var url2 = 'https://atlascove.blob.core.windows.net/images/' + hash + '.geojson??sp=racwdl&st=2021-04-24T19:54:54Z&se=2022-04-25T03:54:54Z&spr=https&sv=2020-02-10&sr=c&sig=nwILb9g1i%2BOB415atZOAfjTOY8KCmfzOLPg46Ut7aXQ%3D'
+  $.ajax({
+      type: 'PUT',
+      url: url2,
+      data: JSON.stringify(json),
+      processData: false,
+      contentType: false,
+      headers: {"Content-Type": "application/json; charset=UTF-8", "x-ms-blob-type": "BlockBlob" }
+  }).done(function(data, xhr) {
+         console.log(data);
+         var status = xhr.status;
+         console.log(status);
+  });
+}
+
+function upload(img,json){
+  console.log(JSON.stringify(json))
+  console.log(json)
+  var hash = json['properties']['id'];
+  var dbx = new Dropbox.Dropbox({ accessToken: 'vmLipsZvDEMAAAAAAAAAAdxF-uncM-lEPVDrE-hJzGcVg-ljIlFLPGl-QUNCpqXJ' });
+  var f = json;
+  //updateWorld(f);
+  $('.modal-content').empty().append('<h2>Uploading...</h2>');
+  $('#submit_button').hide();
+  dbx.filesUpload({path: '/atlascove/' + hash + '.png', contents: img})
+  .then(function(response) {
+    alert('Done!');
+    setTimeout("location.reload(true);", 200);
+    centering = 1;
+    map.scrollZoom.disable();
+    map.setPitch(70);
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+  dbx.filesUpload({path: '/atlascove/' + hash + '.json', contents: JSON.stringify(json)})
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+}
---
+++ cam2/js/map.js
@@ -0,0 +1,121 @@
+var satelliteStyle = {
+    version: 8,
+    sources: {
+        'raster-tiles': {
+            type: 'raster',
+            tiles: [
+                'https://ecn.t0.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=587&mkt=en-gb&n=z',
+                'https://ecn.t1.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=587&mkt=en-gb&n=z',
+                'https://ecn.t2.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=587&mkt=en-gb&n=z',
+                'https://ecn.t3.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=587&mkt=en-gb&n=z',
+            ],
+        },
+    },
+    layers: [
+        {
+            id: 'simple-tiles',
+            type: 'raster',
+            source: 'raster-tiles',
+            minzoom: 0,
+            maxzoom: 22,
+        },
+    ],
+}
+
+marker1 = new maplibregl.Marker({ color: 'blue', draggable: true })
+var marker2 = new maplibregl.Marker({ color: 'red', draggable: true }) // ignore this one, not used
+var map = new maplibregl.Map({
+     container: 'map',
+     style: 'https://api.maptiler.com/maps/streets/style.json?key=LVXocV2Y6nX6vEqq67iz',
+     center: [0, 0],
+     zoom: 12,
+     pitch: 70,
+     attributionControl: false
+});
+
+function map_expand(){
+  $('#map').css('height', '40vh');
+  $('#map').css('top', '60vh');
+}
+
+function center(){
+  if (centering === 1) {
+    navigator.geolocation.getCurrentPosition(function(position){
+      map.jumpTo({
+        center: [position.coords.longitude,position.coords.latitude], zoom: 16
+      });
+    });
+  }
+}
+
+function locate(){
+  if (navigator.geolocation) { //check if geolocation is available
+    console.log('getting user location...');
+    navigator.geolocation.getCurrentPosition(function(position){
+
+    });
+  } else {
+    alert('USER LOCATION NOT AVAILABLE');
+  }
+}
+
+
+
+function addMarker(){
+  if (navigator.geolocation) { //check if geolocation is available
+      navigator.geolocation.getCurrentPosition(function(position){
+        try {
+          marker1.remove()
+        } catch (err){
+          // do nothing
+        }
+        var el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage =
+        'url(img/human.png)';
+        el.style.width = '24px';
+        el.style.height = '24px';
+        marker1 = new maplibregl.Marker(el, {draggable: true})
+        .setLngLat([position.coords.longitude,position.coords.latitude])
+        .addTo(map);
+        originalCoords = [position.coords.longitude,position.coords.latitude];
+        map.jumpTo({
+          center: [position.coords.longitude,position.coords.latitude], zoom: 16
+        });
+      });
+  }
+}
+
+function addLinkOLD(coords){
+  try {
+    marker2.remove()
+    console.log('removing second marker');
+  } catch (err){
+    // do nothing
+  }
+  marker2 = new maplibregl.Marker({ color: 'red', draggable: true })
+  .setLngLat(coords)
+  .addTo(map);
+  console.log(coords);
+}
+
+function addLink(){
+    var coords = map.getCenter();
+    currentCenter = coords;
+    console.log(coords);
+    mapactive = 0;
+}
+
+map.on('load', function () {
+  setInterval(locate, 2000);
+  setInterval(center, 100);
+  setInterval(checkHeading, 3000);
+  map.scrollZoom.disable();
+  center();
+  map.on('click', function (e) {
+    coords =e.lngLat;
+  });
+  // every few seconds relocate users
+  //$('.mapboxgl-ctrl-attrib-button.').attr('aria-pressed','true');
+  //$('.mapboxgl-compact').addClass('mapboxgl-compact-show');
+})
